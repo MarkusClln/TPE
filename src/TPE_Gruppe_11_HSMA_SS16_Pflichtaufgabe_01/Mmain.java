29c0e69b19de786d0f76d8db5e682bcdc28d5eda
Jens <jens.windisch@yahoo.de>
2016-03-16 22:08:03 +0100

    editet class main

---
+++ src/TPE_Gruppe_11_HSMA_SS16_Pflichtaufgabe_01/Mmain.java
@@ -0,0 +1,14 @@
+package TPE_Gruppe_11_HSMA_SS16_Pflichtaufgabe_01;
+
+public class Mmain {
+
+	public static void main(String[] args) {
+		
+		Waehrungen waehrungen = new Waehrungen();
+		
+		System.out.println(waehrungen.euro.toString());
+		
+		System.out.println(waehrungen.dollar.umrechnen(2, waehrungen.euro));
+	}
+
+}
