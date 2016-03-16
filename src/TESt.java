5899ddeb30351c20e4466d180fe81d84f0d369ab
Markus <markus.coellen@gmx.de>
2016-03-16 14:43:29 +0100

    added method Jensi for testings

--- src/TESt.java
+++ src/TESt.java
@@ -1,6 +1,20 @@
-
+import java.util.Scanner;
 public class TESt {
 	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Geben Sie ein ob 1 oder 2");
+		System.out.println(Jensi(sc.nextInt()));
+		sc.close();
+	}
+	private static String Jensi(int a){
+		if(a==1){
+			return "Sie haben keine 1 eingegeben";
+		}else if(a==2){
+			return "Sie haben keine 2 eingegeben";
+		}else{
+			return "Sie haben 1 und 2 eingegeben";
+		}
+		
 		
 	}
 }
