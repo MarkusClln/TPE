627fb4af19007917d3670c486727f9cc19710b07
Jens <jens.windisch@yahoo.de>
2016-03-16 19:19:29 +0100

    added class Waehrungen

---
+++ src/TPE_Gruppe_11_HSMA_SS16_Pflichtaufgabe_01/Waehrungen.java
@@ -0,0 +1,11 @@
+package TPE_Gruppe_11_HSMA_SS16_Pflichtaufgabe_01;
+
+public class Waehrungen {
+
+	protected static Waehrung euro = new Waehrung("Euro", "€", 1.2690);
+	protected static Waehrung yen = new Waehrung("Yen", "Y", 0.0091);
+	protected static Waehrung rub = new Waehrung("Rubel", "RUB", 0.0255);
+	protected static Waehrung chf = new Waehrung("Schweiz", "CHF", 1.0509);
+	protected static Waehrung dollar = new Waehrung("Dollar", "$", 1.0000);
+
+}
