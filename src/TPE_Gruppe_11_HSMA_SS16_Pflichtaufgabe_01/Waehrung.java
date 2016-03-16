7b61cbe77ff4650b9eb8353577b60355c2580780
Jens <jens.windisch@yahoo.de>
2016-03-16 17:51:48 +0100

    added class Waehrung added constructor, getter and methods toString, hashCode, equals added method umrechnen, not finished yet edited main for testing

---
+++ src/TPE_Gruppe_11_HSMA_SS16_Pflichtaufgabe_01/Waehrung.java
@@ -0,0 +1,100 @@
+package TPE_Gruppe_11_HSMA_SS16_Pflichtaufgabe_01;
+
+public class Waehrung {
+	private String name;
+	private String kuerzel;
+	private double kurs;
+
+	Waehrung(String name, String kuerzel, double kurs) {
+		this.name = name;
+		this.kuerzel = kuerzel;
+		this.kurs = kurs;
+	}
+
+	public String getKuerzel() {
+		return kuerzel;
+	}
+
+	public double getKurs() {
+		return kurs;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	/*
+	 * es muss long umrechnen(..) sein und long zurückgeben mit double
+	 * funktioniert die Berechnung einwandfrei, aber laut Aufgabe ist long
+	 * verlangt als Rückgabewert habs mit casten probiert, aber da wird dann nur
+	 * die Stelle vorm Komma zurückgegeben
+	 * 
+	 */
+	public double umrechnen(long betrag, String zielwaehrung) {
+		double umgerechneterBetrag = 0;
+		double dollar;
+
+		dollar = (double) betrag * kurs;
+
+		if (zielwaehrung == "€") {
+			kurs = 1.2690;
+			umgerechneterBetrag = dollar / kurs;
+		} else if (zielwaehrung == "Y") {
+			kurs = 0.0091;
+			umgerechneterBetrag = dollar / kurs;
+		} else if (zielwaehrung == "RUB") {
+			kurs = 0.0255;
+			umgerechneterBetrag = dollar / kurs;
+		} else if (zielwaehrung == "CHF") {
+			kurs = 1.0509;
+			umgerechneterBetrag = dollar / kurs;
+		} else if (zielwaehrung == "$") {
+			umgerechneterBetrag = dollar;
+		} else {
+			System.out.println("Ungütlige Währung");
+		}
+		umgerechneterBetrag = Math.round(umgerechneterBetrag * 10000) / 10000.0;
+		return umgerechneterBetrag;
+	}
+
+	public String toString() {
+		return this.name + " " + "[" + this.kuerzel + "] " + "1 $ = " + this.kurs + " " + this.kuerzel;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kuerzel == null) ? 0 : kuerzel.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Waehrung other = (Waehrung) obj;
+		if (kuerzel == null) {
+			if (other.kuerzel != null)
+				return false;
+		} else if (!kuerzel.equals(other.kuerzel))
+			return false;
+		if (Double.doubleToLongBits(kurs) != Double.doubleToLongBits(other.kurs))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
--- src/TPE_Gruppe_11_HSMA_SS16_Pflichtaufgabe_01/main.java
+++ src/TPE_Gruppe_11_HSMA_SS16_Pflichtaufgabe_01/main.java
@@ -3,8 +3,15 @@ package TPE_Gruppe_11_HSMA_SS16_Pflichtaufgabe_01;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Waehrung euro = new Waehrung("Euro", "€", 1.2690);
+		Waehrung yen = new Waehrung("Yen", "Y", 0.0091);
+		Waehrung rub = new Waehrung("Rubel", "RUB", 0.0255);
+		Waehrung chf = new Waehrung("Schweiz", "CHF", 1.0509);
+		Waehrung dollar = new Waehrung("Dollar", "$", 1.0000);
+		
+		
+		System.out.println(dollar.toString());
+		System.out.println(dollar.umrechnen(1, "€"));
 	}
 
 }
